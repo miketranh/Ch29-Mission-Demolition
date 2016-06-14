77f5db397c1a5b92266d0576b1a2829183c492f1
miketranh <miketranh21@gmail.com>
2016-06-14 15:03:13 -0400

    Final
    Had issue with GitHub once again where it is not saving my commits. I
    don't know why when I open up project it open up a different folder
    versus the one I need to submit to class. Had some issue with the code
    because the book seem to be missing some steps or assuming my current
    knowledge. I tried to look at other code to see my mistakes but continue
    to struggle.

---
+++ Assets/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S;
+	public float easing = 0.15f;
+	public Vector2 minXY;
+	public bool _____________________;
+	public GameObject poi;
+	public float camZ;
+
+	// Use this for initialization
+	void Awake () {
+		S = this;
+		camZ = this.transform.position.z;
+	
+	}//end of Awake()
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Vector3 destination ;
+		if (poi == null) {
+			destination = Vector3.zero;
+		} else {
+			destination = poi.transform.position;
+			if (poi.tag == "Projectile") {
+				if (poi.rigidbody.IsSleeping() ) {
+					poi = null;
+					return;
+				}
+			}
+		}
+
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		this.camera.orthographicSize = destination.y + 10;
+	
+	}//end of Update()
+}//end of FollowCam script
---
+++ Assets/Assets/Scripts/SlingShot.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingShot : MonoBehaviour {
+	static public SlingShot S;
+	public GameObject launchPoint;
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	public bool ______________________________;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+
+	void Awake() {
+		S = this;
+		Transform launchPointTrans = transform.FindChild ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+	}
+
+	// Use this for initialization
+	void OnMouseEnter () {
+		//print ("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void OnMouseExit () {
+		//print ("Slingshot:OnMouseExit()");
+		launchPoint.SetActive (false);
+	}
+	void OnMouseDown() {
+		aimingMode = true;
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.rigidbody.isKinematic = true;
+	}
+	void Update() {
+		//must be aimmode to run this code
+		if (!aimingMode) return;
+		Vector3 mousePos2D = Input.mousePosition; 
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp(0) ){
+			aimingMode = false;
+			projectile.rigidbody.isKinematic= false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			FollowCam.S.poi = projectile;
+			projectile = null;
+			MissionDemolition.ShotFired();
+		}
+}
+}
